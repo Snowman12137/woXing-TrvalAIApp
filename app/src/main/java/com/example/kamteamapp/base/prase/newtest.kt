35369ad345331376b3eb891951e0b1c35d960e27
qc <1553062600@qq.com>
2024-09-02 23:25:23 +0800

    qc_final

---
+++ app/src/main/java/com/example/kamteamapp/base/prase/newtest.kt
@@ -0,0 +1,31 @@
+package com.example.kamteamapp.base.prase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.kamteamapp.base.database.MessageViewModel
+import com.example.kamteamapp.ui.theme.KamTeamAppTheme
+
+class newtest : ComponentActivity() {
+    private val messageViewModel: MessageViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KamTeamAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
