0eccf41ab321f53ad8df434ef633cf33bc799f2a
qc <1553062600@qq.com>
2024-09-02 15:42:17 +0800

    qc_final

--- app/src/main/java/com/example/kamteamapp/base/database/NettoRoom.kt
+++ app/src/main/java/com/example/kamteamapp/base/database/NettoRoom.kt
@@ -1,4 +1,4 @@
-package com.example.kamteamapp.base.network
+package com.example.kamteamapp.base.database
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -21,14 +20,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.kamteamapp.base.network.NetViewModel
+import com.example.kamteamapp.base.network.NetViewModelProvider
+import com.example.kamteamapp.base.network.getresponse
 import com.example.kamteamapp.network.HttpViewModel
-import com.example.kamteamapp.network.MarsPhotosScreen
-import com.example.kamteamapp.network.MarsUiState
 import com.example.kamteamapp.ui.theme.KamTeamAppTheme
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 
 class NettoRoom : ComponentActivity() {
     private val httpViewModel: HttpViewModel by viewModels()
@@ -42,53 +38,38 @@ class NettoRoom : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val nettoRoomViewModel = NettoRoomViewModelProvider(httpViewModel)
+                    val nettoRoomViewModel = NetViewModelProvider(httpViewModel)
                     NettoRoomScreen(nettoRoomViewModel, messageViewModel)
                 }
             }
         }
     }
 }
-
 @Composable
-fun NettoRoomScreen(nettoRoomViewModel: NettoRoomViewModel, messageViewModel: MessageViewModel) {
-    val marsUiState by nettoRoomViewModel.httpViewModel.marsUiState.collectAsState()
-    var number by rememberSaveable { mutableStateOf(0) }
-    val message = nettoRoomViewModel.getreturninformation(marsUiState)
+fun NettoRoomScreen(netViewModel: NetViewModel, messageViewModel: MessageViewModel) {
+    val marsUiState by netViewModel.httpViewModel.marsUiState.collectAsState()
     val allMessageItems by messageViewModel.allMessageItems.collectAsState(initial = emptyList())
+    val all = findallMessageItem(messageViewModel = messageViewModel)
+    val item  = findMessageItem(id = 1, messageViewModel = messageViewModel)
+    var number =1
+    var messagesend = "我们想去西安旅游，我们一共四个人，想玩三天，帮我规划下旅游行程"
     LazyColumn {
-        item { Spacer(modifier = Modifier.height(64.dp)) }
         item {
-            Button(
-                onClick = {
-                    nettoRoomViewModel.getinformation()
-                },
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth()
-                    .height(50.dp)
-            ) {
-                Text(text = "get response")
-            }
-        }
-        item {
-            //插入数据
+            Spacer(modifier = Modifier.height(64.dp))
             Button(onClick = {
-                val testx = Message_item(number,message)
-                messageViewModel.insertMessageItem(testx)
-                number = number+1
-            }, modifier = Modifier
+                NettoRoomInterface(netViewModel = netViewModel, statestate = marsUiState, messagesend = messagesend, messageViewModel = messageViewModel, id = number)
+            },modifier = Modifier
                 .padding(8.dp)
                 .fillMaxWidth()
                 .height(50.dp)
             )
             {
-                Text(text ="插入数据")
+                Text(text ="获取数据并且插入数据库")
             }
         }
         item {
             Button(onClick = {
-                messageViewModel.deleteMessageItem()
+                deleteMessageItem(messageViewModel = messageViewModel)
             },modifier = Modifier
                 .padding(8.dp)
                 .fillMaxWidth()
@@ -99,11 +80,12 @@ fun NettoRoomScreen(nettoRoomViewModel: NettoRoomViewModel, messageViewModel: Me
         }
         item {
             Text(text = "Total items: ${allMessageItems.size}")
+            Text(text = "All items: ${all}")
+            Text(text = "Item: ${item}")
         }
-        item { Text(text = "===============================") }
-        item {
-            Text(text = "Message: $message")
-        }
-    }
 
+    }
 }
+
+
+
--- app/src/main/java/com/example/kamteamapp/base/network/HttpViewModel.kt
+++ app/src/main/java/com/example/kamteamapp/base/network/HttpViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.kamteamapp.network
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.kamteamapp.ui.chat.Message
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,7 +10,7 @@ class HttpViewModel : ViewModel() {
     private val _marsUiState = MutableStateFlow<MarsUiState>(MarsUiState.Loading)
     val marsUiState: StateFlow<MarsUiState> = _marsUiState
 
-    fun getinformation(message: String) {
+    fun sendquestion(message: String) {
         viewModelScope.launch {
             _marsUiState.value = try {
                 val listResult = MarsApi.retrofitService.getinputinformation(
