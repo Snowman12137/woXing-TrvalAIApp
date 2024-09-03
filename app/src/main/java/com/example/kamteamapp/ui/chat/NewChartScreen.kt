b8f33b499054e790c2818d94d70a6abc7b993e62
吴飞宇 <2661275860@qq.com>
2024-09-03 19:50:35 +0800

    test

--- app/src/main/java/com/example/kamteamapp/ui/chat/NewChartScreen.kt
+++ app/src/main/java/com/example/kamteamapp/ui/chat/NewChartScreen.kt
@@ -1,10 +1,14 @@
 package com.example.kamteamapp.ui.chat
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
@@ -16,6 +20,7 @@ fun InitDialog(
 
 ){
     val showingDialog = remember { mutableStateOf(true) }
+    val save_part = remember { mutableStateOf(Formate()) }
     if (showingDialog.value) {
         AlertDialog(
             onDismissRequest = {
@@ -26,7 +31,12 @@ fun InitDialog(
                 Text(text = "基本信息表")
             },
             text = {
-                Text(text = "副标题")
+                Column(
+
+                ) {
+                    //TextAndField("出行人数",save_part.value)
+
+                }
             },
 
             confirmButton = {
@@ -43,3 +53,34 @@ fun InitDialog(
         )
     }
 }
+
+
+@Composable
+fun TextAndField(
+    text:String,
+    index:Int,
+    contents:String
+){
+    Row (
+
+    ){
+        Text(text = text)
+        TextField(
+            value = contents,
+            onValueChange = {newValue->
+                //contents = newValue
+            }
+        )
+    }
+
+
+}
+
+
+data class Formate(
+    var num:String="",
+    var start_part:String="",
+    var end_part:String="",
+    var start_time:String="",
+    var datas:String=""
+)
